693df4f931c95f19a8cad47e76f6540442503561
Huahua <769200142@qq.com>
2019-01-12 22:30:41 +0800

    Push

--- 01-11/MyselfQQ/widget.h
+++ 01-11/MyselfQQ/widget.h
@@ -23,6 +23,19 @@ protected:
     
 private:
     Ui::Widget *ui;
+    
+public:
+   void sndMsg(MsgType type); //广播UDP消息
+   void usrEnter(QString username);//处理新用户加入
+   void usrLeft(QString usrname,QString time); //处理用户离开
+   QString getUsr(); //获取用户名
+   QString getMsg(); //获取聊天信息
+private:
+   QUdpSocket * udpSocket; //udp套接字
+   qint16 port; //端口
+   QString uName; //用户名
+
+  void ReceiveMessage();   //接受UDP消息
 };
 
 #endif // WIDGET_H
