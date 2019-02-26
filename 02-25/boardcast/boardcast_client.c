a897395ef4501fcb667fb11ce53f132d677c3e30
Huahua <769200142@qq.com>
2019-02-26 11:22:17 +0800

    广播通信

---
+++ 02-25/boardcast/boardcast_client.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main()
+{
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(fd==-1)
+    {
+        perror("socket error");
+        exit(1);
+    }
+
+    //绑定自己的IP和端口
+    struct sockaddr_in myself;
+    myself.sin_family = AF_INET;
+    myself.sin_port = htons(6767);
+    int ret = bind(fd, (struct sockaddr*)&myself, sizeof(myself));
+    if(ret==-1)
+    {
+        perror("bind error");
+        exit(1);
+    }
+
+    //接收数据
+    while(1)
+    {
+        char buf[1024] = {0};
+        int len = recvfrom(fd, buf, sizeof(buf), 0, NULL, NULL);
+        if(len==-1)
+        {
+            perror("recvfrom error");
+            exit(1);
+        }
+        else
+        {
+            printf("收到广播，内容为: %s\n", buf);
+        }
+    }
+
+    close(fd);
+
+    return 0;
+}
+
---
+++ 02-25/boardcast/boardcast_server.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main()
+{
+    //创建广播套接字，协议为UDP
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(fd==-1)
+    {
+        perror("socket error");
+        exit(1);
+    }
+
+    //绑定服务器主机IP和端口
+    struct sockaddr_in serv;
+    memset(&serv, 0, sizeof(serv));
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(8787);
+    serv.sin_addr.s_addr = htonl(INADDR_ANY);
+    int ret = bind(fd, (struct sockaddr*)&serv, sizeof(serv));
+    if(ret==-1)
+    {
+        perror("bind error");
+        exit(1);
+    }
+
+    //初始化广播网段和端口信息
+    struct sockaddr_in client;
+    memset(&client, 0, sizeof(client));
+    client.sin_family = AF_INET;
+    client.sin_port = htons(6767);
+    //使用管波地址向客户端发数据,"255.255.255.255"为全局广播    
+    //inet_pton(AF_INET, "255.255.255.255", &client.sin_addr.s_addr);
+    inet_pton(AF_INET, "172.116.175.255", &client.sin_addr.s_addr);
+
+    //服务开放广播权限
+    int flag = 1;
+    setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &flag, sizeof(flag));
+
+    //通信
+    while(1)
+    {
+        //不断发送数据
+        static int num = 0;
+        char buf[1024] = {0};
+        sprintf(buf, "hello UDP BoardCast count = %d", ++num);
+        int ret = sendto(fd, buf, strlen(buf)+1, 0, (struct sockaddr*)&client, sizeof(client));
+        if(ret==-1)
+        {
+            perror("sendto error");
+            exit(1);
+        }
+        printf("服务器向外广播 %d 次\n", num);
+        sleep(1);
+    }
+    close(fd);
+
+    return 0;
+}
+
